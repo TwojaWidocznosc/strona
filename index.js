04ad869bcfcf92a9dfcb922bec0bd18561a84b3a
TwojaWidocznosc <twoja.widocznosc@gmail.com>
2025-06-16 10:03:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,83 +1,61 @@
 var btn = document.querySelectorAll(".option-buttons button.btn")
+var socialButtons = document.querySelectorAll("#oferty div.social-buttons button.btn")
 var packages = document.querySelectorAll("#oferty .offer-card")
 
-btn.forEach(element => {
-    element.addEventListener("click", function(e) {
-        btn.forEach(element => {
-            // console.log(element)
-            element.classList = "btn btn-unactive"
+
+let offersData = [];
+
+async function loadOffers() {
+  const res = await fetch("offers.json");
+  offersData = await res.json();
+}
+
+
+loadOffers().then(() => {
+    socialButtons.forEach(button => {
+        button.addEventListener("click", function(activeButton) {
+            changeOffer(activeButton.srcElement, socialButtons)
         })
-        e.srcElement.classList = "btn btn-primary"
-        if(e.srcElement.id == "strony"){
-            let packages = document.querySelector(".packages")
-            packages.innerHTML = "" 
-            let offers = [
-                ["Portfolio", "499 zł", '<li>Podstawowa strona</li> <li class="unactive">Hosting</li> <li>Domena: 1 rok</li> <li class="unactive">SEO</li> <li class="unactive">Podstrony</li> <li>Dostosowanie mobilne</li> <li class="unactive">Baza danych</li>', false], 
-                ["Standard", "899 zł", '<li>Podstawowa strona</li> <li class="unactive">Hosting</li> <li>Domena: 1 rok</li> <li>SEO</li> <li>1-2 Podstrony</li> <li>Dostosowanie mobilne</li> <li>Podstawowa baza danych</li>', true], 
-                ["Premium", "1499 zł", '<li>Profesjonalna strona</li><li>Hosting: 1 rok</li><li>Domena: 1 rok</li><li>SEO</li><li>5-6 Podstron</li><li>Dostosowanie mobilne</li><li>Baza danych</li>', false]]
-            
-            offers.forEach(offer => {
-                if(offer[3] == true){
-                    packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-                }  else {
-                    packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-                }
-            })
-        }
-        if(e.srcElement.id == "socialmedia"){
-            let packages = document.querySelector(".packages")
-            packages.innerHTML = ""  
-            let offers = [
-                ["IG Start", "149 zł/msc", "", false], 
-                ["IG Normal", "199 zł/msc", "", true], 
-                ["IG Boost", "399 zł/msc", "", false], 
-                ["IG Pro", "599 zł/msc", "", false]]
-            
-            offers.forEach(offer => {
-                if(offer[3] == true){
-                    packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-                }  else {
-                    packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-                }
-            })
-        }
-        if(e.srcElement.id == "niewiem"){
-            let packages = document.querySelector(".packages").innerHTML = "" 
-            let offers = [["Konsultacja", "0 zł", "Napisz do nas maila z informacjami o twojej działalności, a my zaproponujemy spersonalizowaną ofertę dla twojej działalności, która przyciągnie największą ilość klientów"]]
-            
-            offers.forEach(offer => {
-                document.querySelector(".packages").innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <div class="desc">'+offer[2]+'</div>    <a href="" class="btn btn-primary">Kontakt</a>   </div>'
-            })
-        }
     })
+    btn.forEach(element => {
+        element.addEventListener("click", function(e) {
+            changeOffer(e.srcElement, btn)
+        })
+    });
 });
 
-function changeOffer(e) {
-    btn.forEach(element => {
+
+function changeOffer(button, buttonsArray) {
+    let socialButtons = document.querySelector("#oferty div.social-buttons")
+    buttonsArray.forEach(element => {
         // console.log(element)
         element.classList = "btn btn-unactive"
     })
-    e.classList = "btn btn-primary"
-    if(e.id == "strony"){
+    button.classList = "btn btn-primary"
+    if(button.id == "strony"){
+        socialButtons.classList.remove("active")
         let packages = document.querySelector(".packages")
         packages.innerHTML = "" 
-        let offers = [
-            ["Portfolio", "499 zł", '<li>Podstawowa strona</li> <li class="unactive">Hosting</li> <li>Domena: 1 rok</li> <li class="unactive">SEO</li> <li class="unactive">Podstrony</li> <li>Dostosowanie mobilne</li> <li class="unactive">Baza danych</li>', false], 
-            ["Standard", "899 zł", '<li>Podstawowa strona</li> <li class="unactive">Hosting</li> <li>Domena: 1 rok</li> <li>SEO</li> <li>1-2 Podstrony</li> <li>Dostosowanie mobilne</li> <li>Podstawowa baza danych</li>', true], 
-            ["Premium", "1499 zł", '<li>Profesjonalna strona</li><li>Hosting: 1 rok</li><li>Domena: 1 rok</li><li>SEO</li><li>5-6 Podstron</li><li>Dostosowanie mobilne</li><li>Baza danych</li>', false]]
         
-        offers.forEach(offer => {
-            if(offer[3] == true){
-                packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }  else {
-                packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }
+        const siteOffers = offersData.find(x => x.id === "site").offers;
+        
+        siteOffers.forEach(offer => {
+            const featuresHTML = offer.features.join("")
+            const badgeHTML = offer.popular ? '<div class="badge">Najczęściej wybierane</div>' : ""
+
+            packages.innerHTML += '<div class="offer-card"> ${badgeHTML} <div class="title">${offer.title}</div> <div class="price">${offer.price}</div> <ul class="desc">${featuresHTML}</ul> <a href="" class="btn btn-primary">Więcej</a> </div>'
         })
     }
-    if(e.id == "socialmedia"){
+    if(button.id == "socialmedia"){
+        let socialButtons = document.querySelector("#oferty div.social-buttons")
+        socialButtons.classList.add("active")
         let packages = document.querySelector(".packages")
         packages.innerHTML = ""  
-        let offers = [["IG Start", "149 zł/msc", "", false], ["IG Normal", "199 zł/msc", "", true], ["IG Boost", "399 zł/msc", "", false], ["IG Pro", "599 zł/msc", "", false]]
+        let offers = [
+            ["IG Start", "149 zł/msc", "", false], 
+            ["IG Normal", "199 zł/msc", "", true], 
+            ["IG Boost", "399 zł/msc", "", false], 
+            ["IG Pro", "599 zł/msc", "", false]]
         
         offers.forEach(offer => {
             if(offer[3] == true){
@@ -87,12 +65,64 @@ function changeOffer(e) {
             }
         })
     }
-    if(e.id == "niewiem"){
+    if(button.id == "niewiem"){
         let packages = document.querySelector(".packages").innerHTML = "" 
         let offers = [["Konsultacja", "0 zł", "Napisz do nas maila z informacjami o twojej działalności, a my zaproponujemy spersonalizowaną ofertę dla twojej działalności, która przyciągnie największą ilość klientów"]]
-        
+        socialButtons.classList.remove("active")
         offers.forEach(offer => {
             document.querySelector(".packages").innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <div class="desc">'+offer[2]+'</div>    <a href="" class="btn btn-primary">Kontakt</a>   </div>'
+        })
+    }
+
+    if(button.id == "IG"){
+        let packages = document.querySelector(".packages")
+        packages.innerHTML = ""  
+        let offers = [
+            ["IG Start", "149 zł/msc", "", false], 
+            ["IG Normal", "199 zł/msc", "", true], 
+            ["IG Boost", "399 zł/msc", "", false], 
+            ["IG Pro", "599 zł/msc", "", false]]
+        
+        offers.forEach(offer => {
+            if(offer[3] == true){
+                packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
+            }  else {
+                packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
+            }
+        })
+    }
+    if(button.id == "YT"){
+        let packages = document.querySelector(".packages")
+        packages.innerHTML = ""  
+        let offers = [
+            ["YT Start", "149 zł/msc", "", false], 
+            ["YT Normal", "199 zł/msc", "", true], 
+            ["YT Boost", "399 zł/msc", "", false], 
+            ["YT Pro", "599 zł/msc", "", false]]
+        
+        offers.forEach(offer => {
+            if(offer[3] == true){
+                packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
+            }  else {
+                packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
+            }
+        })
+    }
+    if(button.id == "TIKTOK"){
+        let packages = document.querySelector(".packages")
+        packages.innerHTML = ""  
+        let offers = [
+            ["IG Start", "149 zł/msc", "", false], 
+            ["IG Normal", "199 zł/msc", "", true], 
+            ["IG Boost", "399 zł/msc", "", false], 
+            ["IG Pro", "599 zł/msc", "", false]]
+        
+        offers.forEach(offer => {
+            if(offer[3] == true){
+                packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
+            }  else {
+                packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
+            }
         })
     }
 }
