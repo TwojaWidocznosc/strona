72202d484e4ef29139dfbda44e51481cce193fd4
TwojaWidocznosc <twoja.widocznosc@gmail.com>
2025-06-16 10:25:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -26,16 +26,19 @@ loadOffers().then(() => {
 
 
 function changeOffer(button, buttonsArray) {
+    
     let socialButtons = document.querySelector("#oferty div.social-buttons")
+    let packages = document.querySelector(".packages")
+    packages.innerHTML = ""
+
     buttonsArray.forEach(element => {
         // console.log(element)
         element.classList = "btn btn-unactive"
     })
     button.classList = "btn btn-primary"
+    
     if(button.id == "strony"){
         socialButtons.classList.remove("active")
-        let packages = document.querySelector(".packages")
-        packages.innerHTML = "" 
         
         const siteOffers = offersData.find(x => x.id === "site").offers;
         
@@ -54,82 +57,93 @@ function changeOffer(button, buttonsArray) {
         })
     }
     if(button.id == "socialmedia"){
-        let socialButtons = document.querySelector("#oferty div.social-buttons")
         socialButtons.classList.add("active")
-        let packages = document.querySelector(".packages")
-        packages.innerHTML = ""  
-        let offers = [
-            ["IG Start", "149 zł/msc", "", false], 
-            ["IG Normal", "199 zł/msc", "", true], 
-            ["IG Boost", "399 zł/msc", "", false], 
-            ["IG Pro", "599 zł/msc", "", false]]
+
+        const siteOffers = offersData.find(x => x.id === "socialmedia").types.find(x => x.id === "instagram").offers;
         
-        offers.forEach(offer => {
-            if(offer[3] == true){
-                packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }  else {
-                packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }
+        siteOffers.forEach(offer => {
+            const featuresHTML = offer.features.join("")
+            const badgeHTML = offer.popular ? '<div class="badge">Najczęściej wybierane</div>' : ""
+
+            packages.innerHTML += `
+                <div class="offer-card">
+                    ${badgeHTML}
+                    <div class="title">${offer.label}</div>
+                    <div class="price">${offer.price}</div>
+                    <ul class="desc">${featuresHTML}</ul>
+                    <a href="#" class="btn btn-primary">Więcej</a>
+                </div>`
         })
     }
     if(button.id == "niewiem"){
-        let packages = document.querySelector(".packages").innerHTML = "" 
-        let offers = [["Konsultacja", "0 zł", "Napisz do nas maila z informacjami o twojej działalności, a my zaproponujemy spersonalizowaną ofertę dla twojej działalności, która przyciągnie największą ilość klientów"]]
         socialButtons.classList.remove("active")
-        offers.forEach(offer => {
-            document.querySelector(".packages").innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <div class="desc">'+offer[2]+'</div>    <a href="" class="btn btn-primary">Kontakt</a>   </div>'
+
+        const siteOffers = offersData.find(x => x.id === "other").offers;
+        
+        siteOffers.forEach(offer => {
+            const featuresHTML = offer.features.join("")
+            const badgeHTML = offer.popular ? '<div class="badge">Najczęściej wybierane</div>' : ""
+
+            packages.innerHTML += `
+                <div class="offer-card">
+                    ${badgeHTML}
+                    <div class="title">${offer.label}</div>
+                    <div class="price">${offer.price}</div>
+                    <ul class="desc">${featuresHTML}</ul>
+                    <a href="#" class="btn btn-primary">Więcej</a>
+                </div>`
         })
     }
 
     if(button.id == "IG"){
-        let packages = document.querySelector(".packages")
-        packages.innerHTML = ""  
-        let offers = [
-            ["IG Start", "149 zł/msc", "", false], 
-            ["IG Normal", "199 zł/msc", "", true], 
-            ["IG Boost", "399 zł/msc", "", false], 
-            ["IG Pro", "599 zł/msc", "", false]]
+        const siteOffers = offersData.find(x => x.id === "socialmedia").types.find(x => x.id === "instagram").offers;
         
-        offers.forEach(offer => {
-            if(offer[3] == true){
-                packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }  else {
-                packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }
+        siteOffers.forEach(offer => {
+            const featuresHTML = offer.features.join("")
+            const badgeHTML = offer.popular ? '<div class="badge">Najczęściej wybierane</div>' : ""
+
+            packages.innerHTML += `
+                <div class="offer-card">
+                    ${badgeHTML}
+                    <div class="title">${offer.label}</div>
+                    <div class="price">${offer.price}</div>
+                    <ul class="desc">${featuresHTML}</ul>
+                    <a href="#" class="btn btn-primary">Więcej</a>
+                </div>`
         })
     }
     if(button.id == "YT"){
-        let packages = document.querySelector(".packages")
-        packages.innerHTML = ""  
-        let offers = [
-            ["YT Start", "149 zł/msc", "", false], 
-            ["YT Normal", "199 zł/msc", "", true], 
-            ["YT Boost", "399 zł/msc", "", false], 
-            ["YT Pro", "599 zł/msc", "", false]]
+        const siteOffers = offersData.find(x => x.id === "socialmedia").types.find(x => x.id === "youtube").offers;
         
-        offers.forEach(offer => {
-            if(offer[3] == true){
-                packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }  else {
-                packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }
+        siteOffers.forEach(offer => {
+            const featuresHTML = offer.features.join("")
+            const badgeHTML = offer.popular ? '<div class="badge">Najczęściej wybierane</div>' : ""
+
+            packages.innerHTML += `
+                <div class="offer-card">
+                    ${badgeHTML}
+                    <div class="title">${offer.label}</div>
+                    <div class="price">${offer.price}</div>
+                    <ul class="desc">${featuresHTML}</ul>
+                    <a href="#" class="btn btn-primary">Więcej</a>
+                </div>`
         })
     }
     if(button.id == "TIKTOK"){
-        let packages = document.querySelector(".packages")
-        packages.innerHTML = ""  
-        let offers = [
-            ["IG Start", "149 zł/msc", "", false], 
-            ["IG Normal", "199 zł/msc", "", true], 
-            ["IG Boost", "399 zł/msc", "", false], 
-            ["IG Pro", "599 zł/msc", "", false]]
+        const siteOffers = offersData.find(x => x.id === "socialmedia").types.find(x => x.id === "tiktok").offers;
         
-        offers.forEach(offer => {
-            if(offer[3] == true){
-                packages.innerHTML += '<div class="offer-card">  <div class="badge">Najczęściej wybierane</div> <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }  else {
-                packages.innerHTML += '<div class="offer-card">   <div class="title">'+offer[0]+'</div>  <div class="price">'+offer[1]+'</div> <ul class="desc">'+offer[2]+'</ul>    <a href="" class="btn btn-primary">Więcej</a>   </div>'
-            }
+        siteOffers.forEach(offer => {
+            const featuresHTML = offer.features.join("")
+            const badgeHTML = offer.popular ? '<div class="badge">Najczęściej wybierane</div>' : ""
+
+            packages.innerHTML += `
+                <div class="offer-card">
+                    ${badgeHTML}
+                    <div class="title">${offer.label}</div>
+                    <div class="price">${offer.price}</div>
+                    <ul class="desc">${featuresHTML}</ul>
+                    <a href="#" class="btn btn-primary">Więcej</a>
+                </div>`
         })
     }
 }
